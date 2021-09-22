45135bd37c6bbfc7264e24c3b3ff9ed2b4bf75b8
AJ <aj@smartnvm.com>
2021-09-22 23:16:20 +0000

    moved setupInput to input.js and import function

--- play.js
+++ play.js
@@ -1,49 +1,8 @@
 const { connect } = require('./client')
-
-
-const handleUserInput = (key) => {
-  let delay = 10 //ms
-  console.log('log:', key)
-
-  if (key === '8') {
-    return setTimeout(() => ('Move: up'), delay)
-  }
-
-  if (key === '5') {
-    return setTimeout(() => ('Move: down'), delay)
-
-  }
-  if (key === '4') {
-    return setTimeout(() => ('Move: left'), delay)
-
-  }
-  if (key === '6') {
-    return setTimeout(() => ('Move: right'), delay)
-
-  }
-
-  if (key === '\u0003') {
-    process.stdout.write("\u0007");
-    process.exit();
-  }
-
-};
-
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-
-  stdin.on("data", handleUserInput);
-
-  console.log('log:', stdin)
-  return stdin;
-}
-
+const { setupInput } = require('./input')
 
 console.log("Connecting ...");
 
 connect();
 
-module.exports = { setupInput }
+setupInput();
