1d21e46fb25eb74d035c531b1bf953452470f032
AJ <aj@smartnvm.com>
2021-09-22 22:41:11 +0000

    added key event

--- client.js
+++ client.js
@@ -1,26 +1,30 @@
 const net = require("net");
 
+const callbackFun = require('./play.js').setupInput
+
 const stdin = process.stdin;
 stdin.setEncoding('utf8');
 stdin.setRawMode(true);
 
 
 // establishes a connection with the game server
-//135.23.223.133/50542
 const connect = function () {
 
   const conn = net.createConnection({
+    // host: '135.23.223.133',// IP address here,
+    // port: 50542// PORT number here,
+
     host: 'localhost',// IP address here,
-    port: 50541// PORT number here,
+    port: 50541// PORT number her
   });
 
   // interpret incoming data as text
   conn.setEncoding("utf8");
 
   conn.on('connect', (data) => {
-    console.log("Halellujah!!!!!");
+    console.log("Connected! Halellujah!!!!!");
+    conn.write('Name: TRX');
     console.log(data);
-    conn.write('Name: AAJ');
   })
 
   conn.on('data', (data) => {
@@ -34,11 +38,41 @@ const connect = function () {
   })
 
 
+  stdin.on('data', (key) => {
+
+    console.log('')
+    process.stdout.write(key + '....');
+    let delay = 50;
+    if (key === '8') {
+      setTimeout(() => conn.write('Move: up'), delay)
+    }
+
+    if (key === '5') {
+      setTimeout(() => conn.write('Move: down'), delay)
+
+    }
+    if (key === '4') {
+      setTimeout(() => conn.write('Move: left'), delay)
+
+    }
+    if (key === '6') {
+      setTimeout(() => conn.write('Move: right'), delay)
+
+    }
+
+
+    // process.stdout.write(key)
+
+    // \u0003 maps to ctrl+c input
+    if (key === '\u0003') {
+      process.stdout.write("\u0007");
+      process.exit();
+    }
+  })
+
 
   return conn;
 };
-
-
 
 
 module.exports = { connect }
