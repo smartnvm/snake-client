bfbd2d7d19bd595451b079d5d32bf1f535c890bf
AJ <aj@smartnvm.com>
2021-09-23 00:43:47 +0000

    finished challenge

--- client.js
+++ client.js
@@ -1,6 +1,6 @@
 const net = require("net");
 
-const callbackFun = require('./play.js').setupInput
+const callbackFun = require('./input.js').setupInput
 
 const stdin = process.stdin;
 stdin.setEncoding('utf8');
@@ -11,11 +11,10 @@ stdin.setRawMode(true);
 const connect = function () {
 
   const conn = net.createConnection({
-    // host: '135.23.223.133',// IP address here,
-    // port: 50542// PORT number here,
-
-    host: 'localhost',// IP address here,
-    port: 50541// PORT number her
+    host: '135.23.223.133',// IP address here,
+    port: 50542// PORT number here,
+    // host: 'localhost',// IP address here,
+    // port: 50541// PORT number her
   });
 
   // interpret incoming data as text
@@ -35,39 +34,6 @@ const connect = function () {
   conn.write('data', (data) => {
     console.log("DATA CAME IN!!!!!");
     console.log(data);
-  })
-
-
-  stdin.on('data', (key) => {
-
-    console.log('')
-    process.stdout.write(key + '....');
-    let delay = 50;
-    if (key === '8') {
-      setTimeout(() => conn.write('Move: up'), delay)
-    }
-
-    if (key === '5') {
-      setTimeout(() => conn.write('Move: down'), delay)
-
-    }
-    if (key === '4') {
-      setTimeout(() => conn.write('Move: left'), delay)
-
-    }
-    if (key === '6') {
-      setTimeout(() => conn.write('Move: right'), delay)
-
-    }
-
-
-    // process.stdout.write(key)
-
-    // \u0003 maps to ctrl+c input
-    if (key === '\u0003') {
-      process.stdout.write("\u0007");
-      process.exit();
-    }
   })
 
 
--- input.js
+++ input.js
@@ -1,37 +1,68 @@
+let conn
+
+
+
+
+const ctrl = {
+  '8': "Move: up",
+  '4': "Move: left",
+  '5': "Move: down",
+  '6': "Move: right"
+}
 
 const handleUserInput = (key) => {
-  let delay = 10 //ms
-  //console.log(key)
-
-  //console.log('')
-  //process.stdout.write(key + '  handler');
-  console.log('log:', key)
-
-  if (key === '8') {
-    return setTimeout(() => conn.write('Move: up'), delay)
-  }
-
-  if (key === '5') {
-    return setTimeout(() => conn.write('Move: down'), delay)
-
-  }
-  if (key === '4') {
-    return setTimeout(() => conn.write('Move: left'), delay)
-
-  }
-  if (key === '6') {
-    return setTimeout(() => conn.write('Move: right'), delay)
-
-  }
-
+  let delay = 50 //ms
   if (key === '\u0003') {
+    console.log('log:', key)
     process.stdout.write("\u0007");
     process.exit();
   }
 
+
+  if (key === '8') {
+    setTimeout(() => conn.write('Move: up'), delay)
+  }
+
+  if (key === '5') {
+    setTimeout(() => conn.write('Move: down'), delay)
+
+  }
+  if (key === '4') {
+    setTimeout(() => conn.write('Move: left'), delay)
+
+  }
+  if (key === '6') {
+    setTimeout(() => conn.write('Move: right'), delay)
+
+  }
+  if (key === 'w') {
+    setTimeout(() => conn.write('Say: wtf'), delay)
+  }
+  
+  if (key === 'h') {
+    setTimeout(() => conn.write('Say: hello'), delay)
+  }
+  
+  if (key === 'b') {
+    setTimeout(() => conn.write('Say: bye!'), delay)
+  }
+  
+
+  if (key === 'e') {
+    setTimeout(() => conn.write('Say: RUN EBUKA!'), delay)
+  }
+
+
+
+
+
 };
 
-const setupInput = function () {
+
+
+const setupInput = (clientConn) => {
+  conn = clientConn;
+  console.log(conn)
 
   const stdin = process.stdin;
   stdin.setRawMode(true);
@@ -40,9 +71,10 @@ const setupInput = function () {
 
   stdin.on("data", handleUserInput);
 
-  console.log('log:', stdin)
+  //console.log('log:', stdin)
   return stdin;
 }
 
 
+//setupInput();
 module.exports = { setupInput }
