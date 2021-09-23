e9bb35480fb604f842b2304400cece22f1f27e33
AJ <aj@smartnvm.com>
2021-09-23 01:35:18 +0000

    store constant objects for re-use / flexibility

---
+++ constants.js
@@ -0,0 +1,20 @@
+module.exports = {
+  IP: '135.23.223.133',       // IP address here,
+  PORT: 50542,                // PORT number IP
+  CTRL: {
+    '8': "Move: up",
+    '4': "Move: left",
+    '5': "Move: down",
+    '6': "Move: right",
+    'h': "Say: hello",
+    'e': "Say: RUN!",
+    'b': "Say: Bye!",
+    'l': "Say: LH Labs!",
+    'g': "Say: GET OUT!",
+    's': "Say: LOL!",
+    'f': "Say: I see you!",
+    'w': "Say: Back to Work!",
+    'a': "Say: goodnight!"
+  }
+};
+
